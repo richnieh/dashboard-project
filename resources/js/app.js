14ba1c1cb865c591c44af36a96a709b0245b3663
Rich <richnieh@hotmail.com>
2024-08-15 15:39:19 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+import './bootstrap';
+
+import '../admin/js/datatables-simple-demo';
+import '../admin/js/scripts';
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth'])->prefix('admin')->group(function(){
+    Route::get('/', function () {
+        return view('admin.index');
+    })->name('admin.index');
+
+    Route::get('/posts', function(){
+        return view('admin.posts.index');
+    })->name('admin.posts.index');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
